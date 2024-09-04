ae003fda06639c6b422ea24a0e4ce27a888eaa9b
Syed Rayan <srh3182@g.rit.edu>
2024-09-04 10:47:46 +0400

    version1

---
+++ happy_birthday.py
@@ -0,0 +1,9 @@
+def bday_message():
+    name=input("Enter your name")
+    birthmonth=input("Enter your birth month")
+    birthday=input("Enter your birth day")
+    birthyear=input("Enter your birth year")
+    print("Your birthday is on",birthmonth,birthday,",",birthyear,"!")
+bday_message()
+bday_message()
+bday_message()
